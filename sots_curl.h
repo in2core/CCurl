a2104ec711f97e716d558f8204c3e54703c399c9
Michal Tomlein <michal@in2core.com>
2018-06-20 14:14:28 +0200

    Add package description, remove sots_curl.c

--- sots_curl.h
+++ sots_curl.h
@@ -2,23 +2,59 @@
 #define __SOTS_CURL_H
 
 #include <stdbool.h>
+#include <curl/curl.h>
 
-typedef size_t (*curl_func)(void * ptr, size_t size, size_t num, void * ud);
+typedef size_t (*curl_func)(void *ptr, size_t size, size_t num, void *ud);
 
 // functions for curl_easy_setopt
-CURLcode curl_easy_setopt_string(CURL *curl, CURLoption option, const char *param);
-CURLcode curl_easy_setopt_bool(CURL *curl, CURLoption option, bool param);
-CURLcode curl_easy_setopt_func(CURL *handle, CURLoption option, curl_func param);
-CURLcode curl_easy_setopt_pointer(CURL *handle, CURLoption option, void *param);
-CURLcode curl_easy_setopt_long(CURL *handle, CURLoption option, int64_t param);
-CURLcode curl_easy_setopt_slist(CURL *handle, CURLoption option, struct curl_slist *param);
+
+static CURLcode curl_easy_setopt_string(CURL *curl, CURLoption option, const char *param) {
+    return curl_easy_setopt(curl, option, param);
+}
+
+static CURLcode curl_easy_setopt_bool(CURL *curl, CURLoption option, bool param) {
+    return curl_easy_setopt(curl, option, param);
+}
+
+static CURLcode curl_easy_setopt_func(CURL *handle, CURLoption option, curl_func param) {
+    return curl_easy_setopt(handle, option, param);
+}
+
+static CURLcode curl_easy_setopt_pointer(CURL *handle, CURLoption option, void* param)
+{
+    return curl_easy_setopt(handle, option, param);
+}
+
+static CURLcode curl_easy_setopt_long(CURL *curl, CURLoption option, int64_t param) {
+    return curl_easy_setopt(curl, option, param);
+}
+
+static CURLcode curl_easy_setopt_slist(CURL *curl, CURLoption option, struct curl_slist *param) {
+    return curl_easy_setopt(curl, option, param);
+}
 
 // functions for curl_easy_get_info
-CURLcode curl_easy_getinfo_long(CURL *handle, CURLINFO option, int64_t *param);
-CURLcode curl_easy_getinfo_string(CURL *handle, CURLINFO option, const char **param);
-CURLcode curl_easy_getinfo_slist(CURL *handle, CURLINFO option, struct curl_slist **param);
+
+static CURLcode curl_easy_getinfo_long(CURL *handle, CURLINFO option, int64_t *param) {
+    return curl_easy_getinfo(handle, option, param);
+}
+
+static CURLcode curl_easy_getinfo_string(CURL *handle, CURLINFO option, const char **param) {
+    return curl_easy_getinfo(handle, option, param);
+}
+
+static CURLcode curl_easy_getinfo_double(CURL *handle, CURLINFO option, double *param) {
+    return curl_easy_getinfo(handle, option, param);
+}
+
+static CURLcode curl_easy_getinfo_slist(CURL *handle, CURLINFO option, struct curl_slist **param) {
+    return curl_easy_getinfo(handle, option, param);
+}
 
 // functions for curl_get_msg_result
-CURLcode curl_get_msg_result(CURLMsg * msg);
+
+static CURLcode curl_get_msg_result(CURLMsg *msg) {
+    return msg->data.result;
+}
 
 #endif
